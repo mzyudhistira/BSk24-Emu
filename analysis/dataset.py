4b1ed5bcd9709f7b6756b2da65366c9f95db1544
Zhafran <185775768+mzyudhistira@users.noreply.github.com>
2025-06-23 15:20:40 +0200

    skyrme param correlation

--- analysis/dataset.py
+++ analysis/dataset.py
@@ -1,6 +1,30 @@
 import numpy as np
 import pandas as pd
 
+from input.load import load_df
+
+
+def relative_skyrme(result_row):
+    """
+    Calculate the relative skyrme parameter wrt BSk24.
+
+    Args:
+        result_row (pd row): Row of the skyrme param df
+
+    Returns:
+        normalized_skyrme (list): Values of normalized skyrme param
+
+    """
+    bsk_param, _ = load_df("reg", "exp")
+    normalized_skyrme = []
+
+    for i in range(21):
+        normalized_val = result_row[f"param({i+1:02d})"] - bsk_param.iloc[0, i]
+
+        normalized_skyrme.append(normalized_val)
+
+    return normalized_skyrme
+
 
 def extract_result(result_row):
     """
@@ -10,7 +34,7 @@ def extract_result(result_row):
         result_row (pd row): Row of the summary dataframe
 
     Returns:
-        results (dic): Dictionary of RMS and dataset moment
+        results (dict): Dictionary of RMS and dataset moment
 
     """
     mass_table = pd.read_csv(
